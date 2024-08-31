9152b4105657f247d2dac94a32589d901affad23
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2024-08-30 21:00:22 -0300

    Enviando tudo.

--- main.py
+++ main.py
@@ -1,52 +1,50 @@
-import os
-
-state_session_path = 'data/cache/session_state.py'
-
-if not os.path.exists(state_session_path):
-    with open(state_session_path, 'w') as archive:
-        archive.write("logged_user = ''")
-        archive.write("\nlogged_user_password = ''")
-
-
-from dictionary.sql import check_user_query
-from dictionary.vars import to_remove_list
-from functions.login import User
-from functions.create_user import CreateUser
-from functions.query_executor import QueryExecutor
-import streamlit as st
+try:
+    from dictionary.sql import check_user_query
+    from dictionary.vars import to_remove_list
+    from functions.login import User
+    from functions.create_user import CreateUser
+    from functions.query_executor import QueryExecutor
+    import streamlit as st
 
 
-def main():
+    def main():
 
-    st.set_page_config(page_title='Controle Financeiro', page_icon=':moneybag:', layout="wide", initial_sidebar_state="auto", menu_items=None)
+        st.set_page_config(page_title='Controle Financeiro', page_icon=':moneybag:', layout="wide", initial_sidebar_state="auto", menu_items=None)
 
-    hide_streamlit_style = """
-                <style>
-                #MainMenu {visibility: hidden;}
-                footer {visibility: hidden;}
-                </style>
-                """
-    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+        hide_streamlit_style = """
+                    <style>
+                    #MainMenu {visibility: hidden;}
+                    footer {visibility: hidden;}
+                    </style>
+                    """
+        st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
-    query_executor = QueryExecutor()
-    user_quantity = query_executor.simple_consult_query(check_user_query)
-    user_quantity = query_executor.treat_simple_result(user_quantity, to_remove_list)
-    user_quantity = int(user_quantity)
+        query_executor = QueryExecutor()
+        user_quantity = query_executor.simple_consult_query(check_user_query)
+        user_quantity = query_executor.treat_simple_result(user_quantity, to_remove_list)
+        user_quantity = int(user_quantity)
 
-    if user_quantity == 0:
-        create_user = CreateUser()
-        create_user.main_menu()
+        if user_quantity == 0:
+            create_user = CreateUser()
+            create_user.main_menu()
 
-    elif user_quantity > 0:
-        if 'is_logged_in' not in st.session_state:
-            st.session_state.is_logged_in = False
+        elif user_quantity > 0:
+            if 'is_logged_in' not in st.session_state:
+                st.session_state.is_logged_in = False
 
-        if st.session_state.is_logged_in:
-            from screens.app import HomePage
-            HomePage()
-        else:
-            call_user = User()
-            call_user.get_login()
-        
-if __name__ == "__main__":
-    main()
+            if st.session_state.is_logged_in:
+                from screens.app import HomePage
+                HomePage()
+            else:
+                try:
+                    call_user = User()
+                    call_user.get_login()
+                except KeyError:
+                    st.warning(body=":warning: Alguns erros no Login.")
+
+    if __name__ == "__main__":
+        main()
+
+except KeyError:
+    import streamlit as st
+    st.rerun()
