636e846aceaf7e3df415f4e25e3a8d5e893d829b
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2024-09-22 09:29:27 -0300

    Corrigindo o instalador.

--- services/install_service.sh
+++ services/install_service.sh
@@ -1,5 +1,23 @@
+FOLDER=($pwd)
+VENV_NAME="dependencies"
+
+sudo su
+cd $HOME
+python3 -m venv dependencies && cd dependencies
+source ./bin/activate
+cd $FOLDER && cd ..
+pip install -r requirements.txt
+
 echo "#!/bin/bash" >> fcscript.sh
 echo "sleep 1" >> fcscript.sh
+echo "if [ -z "$VIRTUAL_ENV" ]; then" >> fcscript.sh
+echo "    deactivate" >> fcscript.sh
+echo "fi" >> fcscript.sh
+echo "cd $HOME" >> fcscript.sh
+echo "python3 -m venv dependencies && cd dependencies" >> fcscript.sh
+echo "source ./bin/activate" >> fcscript.sh
+echo "cd $FOLDER && cd .." >> fcscript.sh
+echo "pip install -r requirements.txt" >> fcscript.sh
 echo "cd /home/$(whoami)/repos/Finances_Controller/" >> fcscript.sh
 echo "sleep 1" >> fcscript.sh
 echo "streamlit run main.py --server.port 8501" >> fcscript.sh
