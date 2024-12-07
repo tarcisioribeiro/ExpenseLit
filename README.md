824669f9d6cedc8efd7a1a879df737d97d19170a
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2024-12-07 07:28:34 -0300

    Correções necessárias.

--- README.md
+++ README.md
@@ -2,15 +2,15 @@
 
 Um aplicativo de controle financeiro feito em **Python**, através do framework **Streamlit**. Integrado ao banco de dados **MySQL**, permite o controle de receitas e despesas.
 
-## Instalação em ambiente **Linux**
+## Instalação em ambiente GNU/Linux
 
-* Para poder utilizar o sistema, faça a instalação do serviço, abrindo um terminal, e executando os comandos abaixo:
+Pensado a ser executado em ambientes Linux (principalmente distribuições em base Debian) em um primeiro momento, esta aplicação possui uma instalação fácil e rápida, que deve ser feita abrindo um terminal, executando os seguintes comandos:
 
         sudo apt update
         sudo apt upgrade
         sudo apt install git
         git clone https://github.com/tarcisioribeiro/ExpenseLit.git
-        cd Finances_Controller/
+        cd ExpenseLit
         sudo ./services/install_service.sh
 
 A execução do script **install_service.sh** automaticamente realizará a instalação das dependências e configuração do ambiente da aplicação.
--- services/install_service.sh
+++ services/install_service.sh
@@ -17,48 +17,68 @@ FOLDER=$(pwd)
 while true; do
     blue "\nDigite a senha de root:"
     read -s root_password
+    sleep 1
     blue "\nDigite a senha de root novamente: "
     read -s confirm_root_password
+    sleep 1
 
     echo "$root_password" | sudo -S echo "Senha de root aceita."
 
     if [ $? -eq 0 ]; then
         green "\nVocê tem permissões de root. Continuando com o script..."
+        sleep 1
         blue "\nInstalando dependências..."
-        apt install build-essential git neofetch curl wget mysql-server python3-venv python3-tk python3-pip python3.10-full python3.10-dev dkms perl gcc make default-libmysqlclient-dev libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev libncurses5-dev libncursesw5-dev llvm xz-utils tk-dev libffi-dev liblzma-dev python3-openssl -y
+        sleep 1
+        apt install build-essential openssh-server git neofetch curl net-tools wget mysql-server python3-venv python3-tk python3-pip python3.10-full python3.10-dev dkms perl gcc make default-libmysqlclient-dev libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev libncurses5-dev libncursesw5-dev llvm xz-utils tk-dev libffi-dev liblzma-dev python3-openssl -y
         ufw enable
         ufw allow 8501
         ufw allow OpenSSH
         break
     else
         red "\nSenha de root incorreta. Saindo..."
+        sleep 1
         exit 1
     fi
 done
 
+sleep 1
+clear
+
 while true; do
     blue "\nDefina a senha do banco de dados: "
     read -s password
+    sleep 1
     blue "\nRepita a senha: "
     read -s confirmation
+    sleep 1
 
     if [ "$password" = "$confirmation" ]; then
         green "\nSenhas coincidem. Configurando o banco de dados..."
+        sleep 1
         sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$password';"
         cd documentation/
         mysql -u root -p"$password" < implantation_financas.sql
         break
     else
         red "\nAs senhas não coincidem. Tente novamente."
+        sleep 1
     fi
 done
 
+sleep 1
+clear
+
 cd $FOLDER
 blue "\nCriando ambiente virtual..."
+sleep 1
 python3 -m venv venv
 blue "\nAtivando ambiente virtual..."
+sleep 1
 source venv/bin/activate
 pip install -r requirements.txt
+
+sleep 1
+clear
 
 echo "#!/bin/bash" >> fcscript.sh
 echo "cd $FOLDER" >> fcscript.sh
@@ -79,4 +99,9 @@ sudo systemctl enable fcscript.service
 sudo systemctl daemon-reload
 sudo systemctl start fcscript.service
 
-green "\nInstalação concluída.\n"
+green "\nInstalação concluída."
+
+link=$(python3 get_ipv4.py)
+
+blue "Você pode realizar o acesso a aplicação através dos seguintes links:\n"
+green "$link"
